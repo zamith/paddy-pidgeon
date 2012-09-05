1f8cdf34a088a80902cd37884ecc5d08788662f0
Zamith <zamith.28@gmail.com>
2012-09-05 14:20:22 +0100

    Starting paddy pidgeon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaddyPidgeon::Application.config.secret_token = 'a11d7cd666fc88fc07f36484eb3b1b8c7cd754f453d4356647e8e08e9076fb04f60fcf9102fddbe9db3cd9219a2322b3cb167f1fd194123b6e194d054e6246c7'
