0510c1e079cde88461702f031c973f14a97d82c0
Zamith <zamith.28@gmail.com>
2012-09-06 17:23:25 +0100

    Adding unicorn logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,10 @@ PaddyPidgeon::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Unicorn hack to show logs
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
 end
