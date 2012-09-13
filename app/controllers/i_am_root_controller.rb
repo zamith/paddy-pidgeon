837c1ab6ae952cae21a3f7f89f72d4b0118eaed7
Pedro Pereira <pedro.mdp@gmail.com>
2012-09-13 15:02:39 +0100

    * Completed people

--- app/controllers/i_am_root_controller.rb
+++
@@ -1,5 +0,0 @@
-class IAmRootController < ApplicationController
-  def index
-
-  end
-end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,6 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :groups
   belongs_to :user, class_name: "Citygate::User"
 
-  validates :phone_no, presence: true
+  validates :phone_no, presence: true, format: { with: /(\+351|00351)?\d{9}/, message: I18n.t('admin.people.validation.phone_no') }
   validates :user_id, presence: true
 end
