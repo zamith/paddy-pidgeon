6d48f3e0ca26380ecb78df2c0ed95aa88d4384d6
Zamith <zamith.28@gmail.com>
2012-11-30 15:05:40 +0000

    Sending messages again

--- app/models/message.rb
+++ app/models/message.rb
@@ -18,14 +18,13 @@ class Message < ActiveRecord::Base
       'message' => self.text,
       'numbers[]' => phone_numbers
     }
-    p self
     MessageCounter.count(phone_numbers, self.user)
-    #http = Net::HTTP.new(@@url.host, @@url.port)
-    #request = Net::HTTP::Post.new("/form")
-    #request.set_form_data(params)
-    #response = http.request(request)
+    http = Net::HTTP.new(@@url.host, @@url.port)
+    request = Net::HTTP::Post.new("/form")
+    request.set_form_data(params)
+    response = http.request(request)
 
-    #response.code == '200'
+    response.code == '200'
   end
 
   def strip_of_unicode_chars
